0e6b9166d16363dbbaad4498ccb80bcfa01fcbc5
Ngân <thungan16092003@gmail.com>
2023-05-31 22:59:57 +0700

    fix homepage

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,51 @@
 // Put your application javascript here
+document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById("btn")
+    const show = document.getElementById("show")
+    
+    btn.addEventListener("click", function() {
+      if (show.style.display == "none") {
+        show.style.display = "flex"
+        btn.classList.add("transform-rotate")
+      } else {
+        show.style.display = "none"
+        btn.classList.remove("transform-rotate")
+      }
+    })
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById("btn1")
+    const show = document.getElementById("show1")
+    
+    btn.addEventListener("click", function() {
+      if (show.style.display == "none") {
+        show.style.display = "flex"
+        btn.classList.add("transform-rotate")
+      } else {
+        show.style.display = "none"
+        btn.classList.remove("transform-rotate")
+      }
+    })
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById("btn2")
+    const show = document.getElementById("show2")
+  
+    
+    btn.addEventListener("click", function() {
+      if (show.style.display == "none") {
+        show.style.display = "flex"
+        btn.classList.add("transform-rotate")
+      } else {
+        show.style.display = "none"
+        btn.classList.remove("transform-rotate")
+      }
+    })
+  });
+
+  // Put your application javascript here
 
 document.addEventListener("DOMContentLoaded", function () {
   const btn_down = document.getElementById("btn-down");
@@ -20,7 +67,6 @@ document.addEventListener("DOMContentLoaded", function () {
     container_box.classList.add("hidden");
     sort.classList.add("hidden");
   };
-
   const box_show_on = function () {
     container_box.classList.remove("hidden");
   };
@@ -28,7 +74,6 @@ document.addEventListener("DOMContentLoaded", function () {
     container_sort.classList.toggle("container-sort-max");
     container_sort.classList.toggle("container-sort");
   };
-
   sort.addEventListener("click", function () {
     container_box.classList.toggle("hidden");
   });
@@ -38,7 +83,6 @@ document.addEventListener("DOMContentLoaded", function () {
     box_show_off();
     change_box_size();
   });
-
   btn_up.addEventListener("click", function () {
     up.classList.remove("hidden");
     box_show_off();
@@ -54,7 +98,6 @@ document.addEventListener("DOMContentLoaded", function () {
     box_show_off();
     change_box_size();
   });
-
   down.addEventListener("click", function () {
     box_show_on();
     down.classList.add("hidden");
@@ -67,7 +110,6 @@ document.addEventListener("DOMContentLoaded", function () {
     sort.classList.remove("hidden");
     change_box_size();
   });
-
   sort_down.addEventListener("click", function () {
     box_show_on();
     sort_down.classList.add("hidden");
